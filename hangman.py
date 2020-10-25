8a91d90226dfe80ec841855477ec4b5c616381ed
Cristina Arenas Izquierdo <cristina.arenas.izq@gmail.com>
2020-10-25 16:04:47 +0100

    Choose function done

--- hangman.py
+++ hangman.py
@@ -11,17 +11,25 @@ print(choosen_word)
 
 # Functions that will be used
 count = 0
-def choose(count1):
+def choose(count):
     ''' User interation. Choose a letter and check if it is OK.'''
     letter= input("Choose a letter: ")
     if (letter in choosen_word or letter in choosen_word.swapcase()):
         print('Good job!')
         print(hangman[count])
+        
     else:
         print('Sorry, try again :(')
         count += 1
         print(hangman[count])
     return count
+
+#while user_word != choosen_word:
+
+
+for i in range(8):
+    count = choose(count)
+
 '''
 def letter_in_word():
     print('Good job!')
@@ -33,6 +41,3 @@ def letter_not_in_word():
     count += 1
     return count
 '''
-#while user_word != choosen_word:
-for i in range(4):
-    choose(count)
